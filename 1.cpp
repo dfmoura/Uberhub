bb507d7615d17735623a25b6efda12c2ee3a6d8a
Diogo Moura <diogomou@gmail.com>
2023-03-06 06:11:02 -0300

    Revisao para fazer Iniciante 2

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    cout << "O valor de x eh: "<< x << endl;
+    for(int i = 0; i < x;i++){
+        cout << i << " ";
+    }
+
+}
