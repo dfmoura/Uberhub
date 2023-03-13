e331345e07a536379f6b1358c869173b6e618b8f
Diogo Moura <diogomou@gmail.com>
2023-03-12 22:34:13 -0300

    2 = Estuto de vetores

---
+++ 12.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//ler 10 valores inteiros
+//mostrar os 10 valores
+//calcular a soma dos valores
+//trocar o valor que esta na posicao 7 do vetor para o valor 353
+//calcular a soma novamente
+//mostar o vetor na tela
+
+    int main(){
+        int antonio[10];
+        
+        for(int i=0; i<10; i++){
+            cin >> antonio[i];
+        }
+        for(int i =0; i<10;i++){
+            cout<<antonio[i]<< " ";
+        }
+        cout<<endl;
+        int soma = 0;
+        for(int i = 0;i<10; i++){
+            soma = soma + antonio[i];
+        }
+        cout<< "A soma dos valores do vetor eh "<< soma << endl;
+        antonio[7] = 353;
+        for(int i=0;i<10;i++){
+            cout<<antonio[i]<<" ";
+        }
+        cout << endl;
+        soma = 0;
+        for(int i = 0; i<10; i++){
+            soma = soma + antonio[i];
+        }
+        cout<<"lA nova soma dos valores do vetor eh " << soma << endl;
+        for(int i = 0;i<10;i++){
+            cout<< antonio[i]<<" ";
+        }
+        cout<< endl;
+
+
+    }
+    
