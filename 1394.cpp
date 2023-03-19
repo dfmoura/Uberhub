6327c2216694651c79ae705277b0e1ff292cea79
Diogo Moura <diogomou@gmail.com>
2023-03-19 07:41:33 -0300

    string e vetores

---
+++ 1394.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str, str1;
+    getline(cin,str);
+    str1=str;
+    reverse(str1.begin(), str1.end());    
+    if(str == str1){
+        cout<< "eh palindromo"<<endl;
+    }else{
+        cout<<"nao eh palindromo"<<endl;
+    }
+
+}
