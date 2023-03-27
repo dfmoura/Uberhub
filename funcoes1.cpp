87f5aee43a9613c42f36f7fedf3b0e6d2bf99320
Diogo Moura <diogomou@gmail.com>
2023-03-27 05:40:45 -0300

    funcoes

---
+++ funcoes1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int soma_posicoes(vector<int>vet){
+    int soma = 0;
+    for(int i = 0; i< vet.size();i++){
+        soma += vet.at(i);
+    }
+    return soma;
+}
+
+
+int main(){
+
+    soma_posicoes
+
+}
